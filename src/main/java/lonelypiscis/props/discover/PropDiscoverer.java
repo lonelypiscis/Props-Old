d912fbf270711b0b4007d12685497c1dec0cb7b9
lonelypiscis <gurokrei@gmail.com>
2017-03-07 20:25:39 +0100

    Further development

--- src/main/java/lonelypiscis/props/discover/PropDiscoverer.java
+++ src/main/java/lonelypiscis/props/discover/PropDiscoverer.java
@@ -1,40 +1,106 @@
 package lonelypiscis.props.discover;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Optional;
+
+import javax.annotation.Resource;
 
 import org.spongepowered.api.Sponge;
+import org.spongepowered.api.data.property.item.HarvestingProperty;
+import org.spongepowered.api.data.property.item.UseLimitProperty;
+import org.spongepowered.api.item.ItemType;
+import org.spongepowered.api.item.ItemTypes;
 
+import lonelypiscis.props.Props;
+import lonelypiscis.props.prop.ModelData;
+import lonelypiscis.props.prop.Prop;
 import lonelypiscis.props.utils.Debug;
+import lonelypiscis.props.utils.ResourceUtils;
 import ninja.leaping.configurate.ConfigurationNode;
-import ninja.leaping.configurate.ConfigurationOptions;
 import ninja.leaping.configurate.commented.CommentedConfigurationNode;
 import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
 import ninja.leaping.configurate.loader.ConfigurationLoader;
 
+/*
+ * The PropDiscoverer scans the prop configurations directory. Found files are checked and then loaded into
+ * the Re
+ */
+
 public class PropDiscoverer {
-	public static Path discoveryFilePath = Paths.get(Sponge.getAssetManager().getAsset("discovery.conf").get().getUrl().toString());
-	ConfigurationLoader<CommentedConfigurationNode> loader;
+	private File propConfigurationsDir;
 	
 	public PropDiscoverer() {
-		
+		propConfigurationsDir = ResourceUtils.getAssetFile("propConfigurations/");
 	}
 	
+	/**
+	 * Invokes the discovery process. Discovered files are handled by the {@link #handleDiscovery(Prop) handleDiscovery} method.
+	 */
+	
 	public void discover() {
-		Debug.getLogger().info("DiscoveryFilePath = " + discoveryFilePath);
+		for (File fileToScan : scanDirectory(propConfigurationsDir)) {
+			Prop discoveredProp = readPropFile(fileToScan);
+			handleDiscovery(discoveredProp);
+		}
+	}
+	
+	private void handleDiscovery(Prop prop) {
+		ModelData loadedModelData = prop.getModelData();
 		
-		loader = HoconConfigurationLoader.builder().setPath(discoveryFilePath).build();
-		ConfigurationNode rootNode = loader.createEmptyNode(ConfigurationOptions.defaults());
+		if (loadedModelData.getOverrideTarget() == null) {
+			Debug.getLogger().info("Requesting model data...");
+			
+			// Apply for resource pack
+			prop.setModelData(Props.getProps().getResourcePackFactory().requestModelData(loadedModelData.getModelName()));
+			// Notify PropRegistry
+			Props.getProps().getRegistry().registerDiscoveredProp(prop, false);
+		}
+	}
+	
+	/**
+	 * Reads and parses a prop configuration file into a Prop object.
+	 */
+	
+	public Prop readPropFile(File fileToScan) {
+		Prop propOut = new Prop();
+		ConfigurationLoader<CommentedConfigurationNode> loader = HoconConfigurationLoader.builder().setFile(fileToScan).build();
 		
 		try {
-		    rootNode = loader.load();
-		} catch(IOException e) {
-		    e.printStackTrace();
+			ConfigurationNode rootNode = loader.load();
+			propOut.loadFromNode(rootNode);
+			Debug.getLogger().info("Scanned " + propOut.getId());
+			
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		
-		boolean shouldDiscover = rootNode.getNode("discover", "autodiscover").getBoolean();
+		return propOut;
+	}
+	
+	/** 
+	 * Scans the given directory for suitable prop configuration files. 
+	 */
+	
+	public ArrayList<File> scanDirectory(File scanDirectory) {
+		ArrayList<File> filesOut = new ArrayList<>();
 		
-		System.out.println(shouldDiscover);
+		for (File foundFile : scanDirectory.listFiles()) {
+			// Check the found item's criteria. Only files with an conf extension are allowed
+			
+			String fileName = foundFile.getName();
+			String fileExtension = fileName.substring(fileName.length()-4, fileName.length());
+			
+			// Is directory?
+			if (!foundFile.isDirectory()) {
+				// Has propery extension?
+				if (fileExtension.equals("conf")) {
+					filesOut.add(foundFile);
+				}
+			}
+		}
+		
+		return filesOut;
 	}
 }
